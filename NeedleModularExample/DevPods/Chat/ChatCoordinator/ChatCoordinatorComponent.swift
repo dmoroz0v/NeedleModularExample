ae496424ad26c84f17a7cffee5df7b0ba975e6bc
kramig <kramig@yandex-team.ru>
2024-06-24 12:08:23 +0500

    fix

--- NeedleModularExample/DevPods/Chat/ChatCoordinator/ChatCoordinatorComponent.swift
+++ NeedleModularExample/DevPods/Chat/ChatCoordinator/ChatCoordinatorComponent.swift
@@ -26,9 +26,9 @@ public class ChatCoordinatorComponent: BootstrapComponent, IChatCoordinatorCompo
         ChatListComponent(parent: self)
     }
     
-    public lazy var chatService: IChatService = {
-        ChatService()
-    }()
+    public var chatService: IChatService {
+        shared { ChatService() }
+    }
     
     public var logger: ILogger {
         dependencies.logger
